7255a90f68f82ce32e47c069e6941917c604c5bd
Rahil-Huseynov <rahil.huseynov.05@gmail.com>
2024-03-07 19:05:20 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+// Task - 1
+
+// let num1 = +prompt('Eni')
+
+// let num2 = +prompt('Uzunluq')
+
+// function sahe() {
+//     let number1 = Math.abs(num1)
+//     let number2 = Math.abs(num2)
+//     let S = number1 * number2
+
+//     if (num1 < 0 || num2 < 0) {
+//         alert("Menfi olcu olmur, Musbete cevirlecek :)")
+//     }
+
+//     alert("Sahəsi:" + S)
+// }
+
+// sahe()
+
+
+// Task - 2
+
+
+// function random() {
+
+//     let r = Math.floor(Math.random() * 2);
+
+//     let g = Math.floor(Math.random() * 5);
+
+//     let b = Math.floor(Math.random() * 256);
+
+
+
+//     setInterval(() => {
+//         const now = new Date();
+
+//         time.innerHTML = (now.toLocaleTimeString())
+
+//     }, 1000);
+
+//     return `rgb(${r},${g},${b})`
+
+// }
+
+
+// let div = document.getElementById('div')
+
+// let time = document.getElementById('time')
+
+// let history = document.getElementById('history')
+
+// setInterval(() => {
+//     div.style.background = random();
+// }, 1000);
